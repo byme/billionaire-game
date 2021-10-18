c29d50947022ca9a538a53b477ef3a1118156abe
byme <caballerof204@gmail.com>
2021-10-17 20:53:51 -0500

    Generacion de las funcionalidades del juego en la clase Juego

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,24 @@
 
 import connection.conectar;
 import controller.Consulta;
+import model.Juego;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        String respuesta="";
+        Juego game=new Juego();
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+        respuesta=reader.readLine();
+
+
 
     }
 }
