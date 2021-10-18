5e0ec131373e9e71fb26a0e661b214e78ffa5434
byme <caballerof204@gmail.com>
2021-10-17 22:19:01 -0500

    Creación de la lógica para final para dirigir el juego

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,12 +12,29 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String respuesta="";
+        String respuesta,aux="";
         Juego game=new Juego();
-        BufferedReader reader =
-                new BufferedReader(new InputStreamReader(System.in));
-        respuesta=reader.readLine();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        game.mensajeIngresoNombre();
+        respuesta= reader.readLine();
+        game.asignarUsuario(respuesta);
 
+
+        do{
+            game.asignarPregunta();
+            game.mostrarPregunta();
+            respuesta= reader.readLine();
+            while (!game.validarSintaxisRespuesta(respuesta)){
+                game.mensajeDeReingresoDato();
+                respuesta= reader.readLine();
+            }
+            aux=game.validarRespuesta(respuesta);
+            game.actualizarEstadoJuego(aux);
+        }while(!game.getEstadoJuego());
+        if (game.isJuegoGanado())
+            game.ganador();
+
+        System.out.println("Fin del juego");
 
 
     }
